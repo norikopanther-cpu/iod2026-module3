3569c299c654973ad57309e097bc3c2b77f21751
Noriko-Panther <noriko.panther@canterbury.ac.nz>
2026-02-24 19:37:12 +1300

    initial commit

---
+++ LabExercises/JSFundamentals.js
@@ -0,0 +1,54 @@
+//Q1
+console.log("" + 1 + 0);
+console.log("" - 1 + 0);
+console.log(true + false);
+console.log(6 / "3");
+console.log("2" * "3");
+console.log(4 + 5 + "px");
+console.log("$" + 4 + 5);
+console.log("4" - 2);
+console.log("4px" - 2);
+console.log("-9" + 5);
+console.log("-9" - 5);
+console.log(null + 1);
+console.log(undefined + 1);
+console.log(" \t \n" - 2);  
+
+//Q2
+let three = "3"
+let four = "4"
+let thirty = "30"
+//what is the value of the following expressions?
+let addition = three + four
+let multiplication = three * four
+let division = three / four
+let subtraction = three - four
+let lessThan1 = three < four
+let lessThan2 = thirty < four
+console.log(addition);
+console.log(multiplication);
+console.log(division);
+console.log(subtraction);
+console.log(lessThan1);
+console.log(lessThan2); 
+//fix the problems
+let additionFixed = Number(three) + Number(four)
+let multiplicationFixed = Number(three) * Number(four)
+let divisionFixed = Number(three) / Number(four)
+let subtractionFixed = Number(three) - Number(four)
+let lessThan1Fixed = Number(three) < Number(four)
+let lessThan2Fixed = Number(thirty) < Number(four)
+console.log(additionFixed);
+console.log(multiplicationFixed);
+console.log(divisionFixed);
+console.log(subtractionFixed);
+console.log(lessThan1Fixed);
+console.log(lessThan2Fixed);    
+
+//Q3
+if (0) console.log('#1 zero is true')
+if ("0") console.log('#2 zero is true')
+if (null) console.log('null is true')
+if (-1) console.log('negative is true')
+if (1) console.log('positive is true')
+    
