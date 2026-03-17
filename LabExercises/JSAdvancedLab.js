6ae9057448a9b2a3d4f65fa52d95c5e7bea9e184
Noriko-Panther <noriko.panther@canterbury.ac.nz>
2026-03-17 20:22:43 +1300

    Adv lab updates

---
+++ LabExercises/JSAdvancedLab.js
@@ -0,0 +1,90 @@
+//alert("JavaScript is working!");
+
+//Q1
+/*A decorator function is a function that wraps another function and adds extra behaviour without changing the original function.
+A decorator takes a function, enhances it, and returns a new function.*/
+
+function makeCounter() { 
+    let currentCount = 0; 
+    return function() { 
+        currentCount++; 
+        console.log(currentCount) 
+        /*document.getElementById("counter1").innerHTML += currentCount + "<br>" ;*/
+        return currentCount; 
+    }; } 
+    
+    let counter1 = makeCounter(); 
+    counter1(); // 1 
+    counter1(); // 2
+
+    let counter2 = makeCounter();
+    counter2(); // 1 
+    counter2(); // 2 
+    counter1(); // 3
+
+function makeCounterStartFrom(startFrom) {
+    let currentCount = startFrom; 
+    return function() { 
+        currentCount++; 
+        console.log(currentCount) 
+        return currentCount; 
+    }; } 
+    
+    let counter3 = makeCounterStartFrom(5); 
+    counter3(); // 6 
+    counter3(); // 7
+
+function makeCounterIncrementBy(startFrom, incrementBy) {
+    let currentCount = startFrom; 
+    return function() { 
+        currentCount += incrementBy; 
+        console.log(currentCount) 
+        return currentCount; 
+    }; } 
+    
+    let counter4 = makeCounterIncrementBy(10, 2); 
+    counter4(); // 12 
+    counter4(); // 14
+
+/*Q2.
+The following delayMsg function is intended to be used to delay printing a message until some time has passed.
+a) What order will the four tests below print in? Why?
+b) Rewrite delayMsg as an arrow function
+c) Add a fifth test which uses a large delay time (greater than 10 seconds)
+d) Use clearTimeout to prevent the fifth test from printing at all. */
+
+function delayMsg(msg) { 
+    console.log(`This message will be printed after a delay: ${msg}`) 
+} 
+setTimeout(delayMsg, 100, '#1: Delayed by 100ms');  //3rd
+setTimeout(delayMsg, 20, '#2: Delayed by 20ms'); //2nd
+setTimeout(delayMsg, 0, '#3: Delayed by 0ms'); //1st
+delayMsg('#4: Not delayed at all'); 
+
+let timerId = setTimeout(() => {
+    delayMsg('#5: Delayed by 10 seconds');
+}, 10000);
+clearTimeout(timerId);
+
+
+//Q3
+function debounce(func, ms) { // create a function 'debounce' with 'func' that I want to control and 'ms' (how long to wait)
+let timerId; // store timerId: 'Which timer is currently active?'
+  return function (...args) { // when someone calls this function...
+    clearTimeout(timerId); // cancel any previous waiing actions
+    timerId = setTimeout(() => { // start a new waiting timer
+      func(...args); // when the waiting finishes, run the function
+    }, ms); // wait for ms milliseconds before running the function
+  };
+}
+
+function printMe(msg) {
+  console.log(`printing debounced message: ${msg}`);
+}
+
+printMe = debounce(printMe, 1000);
+
+// fire off 3 calls within 300ms
+setTimeout(() => printMe('first call'), 100);
+setTimeout(() => printMe('second call'), 200);
+setTimeout(() => printMe('third call'), 300);
